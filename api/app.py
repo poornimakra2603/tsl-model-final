201d35a4d1b2b025e18783ac24b4342515d6ded1
poornimakra2603 <112956034+poornimakra2603@users.noreply.github.com>
2025-06-09 13:24:14 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -67,6 +67,6 @@ def predict():
     except Exception as e:
         print(f"Error in prediction: {str(e)}")
         return jsonify({"error": str(e)}), 500
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=5000)
